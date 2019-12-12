26fa511010f5e3881dfac3c5b6d18fbffdc5f1ad
gaog <yunluoliushi@163.com>
2019-12-12 10:30:21 +0800

    init

---
+++ src/main/java/com/xnpool/scheduler/quartz/mapper/JobEntityMapper.java
@@ -0,0 +1,25 @@
+/**
+ * 版权声明： 版权所有 违者必究 2017
+*/
+package com.xnpool.scheduler.quartz.mapper;
+
+import com.xnpool.scheduler.quartz.entity.JobEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface JobEntityMapper {
+    
+	int countByExample(JobEntity record);
+
+    List<JobEntity> selectByExample(JobEntity record);
+    
+	JobEntity selectByPrimaryKey(Integer id);
+
+	int insertSelective(JobEntity record);
+
+	int updateByPrimaryKey(JobEntity record);
+	
+	int deleteByPrimaryKey(Integer id);
+}
